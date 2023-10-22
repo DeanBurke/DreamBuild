85d7f59a408c1d93830bc426126180ec611ef75d
DeanBurke <deanburke07@hotmail.com>
2023-10-22 15:01:16 +0000

    First deployment to heroku

--- dreambuild/settings.py
+++ dreambuild/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-_6=9bs5##n5vcw7ux^j+p&ki3z0r)u!ahj-84x*=&-c43ro_d#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-deanburke-dreambuild-gcwb3q45swp.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['dreambuildfitness-7af9d60474bd.herokuapp.com', '8000-deanburke-dreambuild-gcwb3q45swp.ws-eu105.gitpod.io']
 
 
 # Application definition
@@ -121,12 +122,18 @@ WSGI_APPLICATION = 'dreambuild.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
