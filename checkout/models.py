fa99c229a2e2e3024d05a6cd74fe3d9c8ef4b0d4
DeanBurke <deanburke07@hotmail.com>
2023-12-05 18:13:46 +0000

    Added discount spend limit to order model and update total

--- checkout/models.py
+++ checkout/models.py
@@ -27,6 +27,8 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, default=0)
+    discount_spend = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(
@@ -48,11 +50,15 @@ class Order(models.Model):
         """
         self.order_total = self.lineitems.aggregate(
             Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        if self.order_total > settings.ORDER_DISCOUNT:
+            self.discount_spend = self.order_total * settings.DISCOUNT_PERCENTAGE / 100
+        else:
+            self.discount_spend = 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
-        self.grand_total = self.order_total + self.delivery_cost
+        self.grand_total = self.order_total + self.delivery_cost - self.discount_spend
         self.save()
 
     def save(self, *args, **kwargs):
